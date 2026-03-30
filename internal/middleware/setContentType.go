595ff71b2d71ddfdcbc898308389fe71ae3bb342
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-03-30 09:19:22 -0400

    Fix: Set content type after handler execution

--- internal/middleware/setContentType.go
+++ internal/middleware/setContentType.go
@@ -4,9 +4,9 @@ import "net/http"
 
 func SetContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
 		if w.Header().Get("Content-Type") == "" {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		}
-		next.ServeHTTP(w, r)
 	})
 }
